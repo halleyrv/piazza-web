a2c414c258dceb098e5886444d93b042008a70d4
Halle <francrv@hotmail.com>
2023-12-25 16:40:52 -0500

    Build the authentication system

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "requres a name" do
+  test "requires a name" do
     @user = User.new(name: "", email: "johndoe@example.com", password: "password")
     assert_not @user.valid?
     @user.name = "John"
@@ -44,5 +44,44 @@ class UserTest < ActiveSupport::TestCase
     max_length = ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED
     @user.password = "a" * (max_length +1)
     assert_not @user.valid?
+  end
+
+  test "can create a session with email and correct password" do
+    @app_session = User.create_app_session(
+      email: "jerry@example.com",
+      password: "password"
+    )
+    assert_not_nil @app_session
+    assert_not_nil @app_session.token
+  end
+
+  test "cannot create a session with email and incorrect password" do
+    @app_session = User.create_app_session(
+      email: "jerry@example.com",
+      password: "wrong"
+    )
+    assert_nil @session
+  end
+
+  test "creating a session with non existent email returns nil" do
+    @app_session = User.create_app_session(
+      email: "whoam@example.com",
+      password: "WRONG"
+    )
+    assert_nil @app_session
+  end
+
+  test "can authenticate with a valid session id and token" do
+    @user = users(:jerry)
+    @app_session = @user.app_sessions.create
+
+    assert_equal @app_session, 
+      @user.authenticate_app_session(@app_session.id, @app_session.token)
+  end
+
+  test "trying to authenticate with a token that doesn't exist returns false" do
+    @user = users(:jerry)
+
+    assert_not @user.authenticate_app_session(50,"token")
   end
 end
